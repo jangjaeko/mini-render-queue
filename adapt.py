a472b7ff10e8c3f1a00e9acba1c460200452dd98
unknown <jangjaeko45@gmail.com>
2025-12-15 22:03:08 -0800

    adapt pattern

---
+++ adapt.py
@@ -0,0 +1,22 @@
+import socket
+
+class FileLikeSocket:
+    def __init__(self, sock):
+        self.sock = sock
+
+    def write(self, message_and_newline):
+        self.sock.sendall(message_and_newline.encode('ascii'))
+
+    def flush(self):
+        pass
+
+class FileLikeSyslog:
+    def __init__(self, priority):
+        self.priority = priority
+
+    def write(self, message_and_newline):
+        message = message_and_newline.rstrip('\n')
+        syslog.syslog(self.priority, message)
+
+    def flush(self):
+        pass
